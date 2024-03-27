2783fb82b5736589b9673ef7e070757635dae817
papito <papito@Ubuntu2.myguest.virtualbox.org>
2024-03-27 20:45:43 +0300

    review5

---
+++ review5/go.mod
@@ -0,0 +1,5 @@
+module review5
+
+go 1.19
+
+require github.com/go-chi/chi v1.5.5
---
+++ review5/main.go
@@ -0,0 +1,84 @@
+package main
+
+// Написать сервер, отправляющий запрос на API https://demo.apistubs.io/api/v1/users
+// и возвращающий общую структуру пользователя с данными профиля,без staticData
+// Если сумма на аккаунте превышает 50000 скрыть email, username, firstName, lastName, avatar
+import (
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"github.com/go-chi/chi"
+	"log"
+	"net/http"
+)
+
+type Object struct {
+	Records     []Record `json:"id"`
+	Skip        int      `json:"skip"`
+	Limit       int      `json:"limit"`
+	TotalAmount int      `json:"totalAmount"`
+}
+
+type Record struct {
+	ID        int     `json:"id"`
+	Email     string  `json:"email"`
+	Profile   Profile `json:"profile"`
+	Password  string  `json:"password"`
+	Username  string
+	CreatedAt string `json:"createdAt"`
+	CreatedBy string
+}
+
+type Profile struct {
+	Dob        string `json:"dob"`
+	Avatar     string `json:"avatar"`
+	LastName   string `json:"lastName"`
+	Firstname  string `json:"firstname"`
+	StaticData string `json:"staticData"`
+}
+
+// Если сумма на аккаунте превышает 50000 скрыть email, username, firstName, lastName, avatar
+func main() {
+	r := chi.NewRouter()
+	r.Get("/users", getUsers)
+	fmt.Println("Сервер запущен на порту :8080")
+	err := http.ListenAndServe(":8080", r)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	var Obj Object
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
+
+	// Отправляем GET-запрос с использованием созданного клиента
+	resp, err := client.Get("https://demo.apistubs.io/api/v1/users")
+	if err != nil {
+		http.Error(w, "Ошибка при получении данных", http.StatusInternalServerError)
+		log.Fatal(err)
+	}
+	err = json.NewDecoder(resp.Body).Decode(&Obj)
+	if err != nil {
+		http.Error(w, "Ошибка при сериализации данных", http.StatusInternalServerError)
+		return
+	}
+
+	if Obj.TotalAmount > 50000 {
+		for _, j := range Obj.Records {
+			j.Email = ""
+			j.Username = ""
+			j.Profile.Firstname = ""
+			j.Profile.LastName = ""
+			j.Profile.Avatar = ""
+		}
+	}
+	err = json.NewEncoder(w).Encode(Obj)
+	if err != nil {
+		return
+	}
+}
